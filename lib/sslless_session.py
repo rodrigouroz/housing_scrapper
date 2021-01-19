4a85367503cf92a6209ac11618c819e8a5c6a354
Rodrigo Uroz <ruroz@medallia.com>
2021-01-19 10:26:22 -0300

    adds a session object that bypasses ssl for telegram and the providers

--- lib/sslless_session.py
+++ lib/sslless_session.py
@@ -2,6 +2,12 @@ from requests import Session
 from lib.hostname_ignoring_adapter import HostNameIgnoringAdapter
 
 class SSLlessSession(Session):
+    def __init__(self) -> None:
+        super().__init__()
+        self.mount('https://', HostNameIgnoringAdapter())
+    def get(self, url, **kwargs):
+        kwargs.setdefault('verify', False)
+        return super().get(url, **kwargs)
     def post(self, url, data, **kwargs):
         kwargs.setdefault('verify', False)
         # this will fail is there is no response, so this is assuming the happy path
