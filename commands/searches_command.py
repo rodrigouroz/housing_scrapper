400de33eea90894dbaa1581823c7f2b89a9f43ab
Mariano Fresno <mnofresno@gmail.com>
2021-01-25 17:31:15 -0300

    Fix message too long bad request error using a message by provider to reply searches

--- commands/searches_command.py
+++ commands/searches_command.py
@@ -2,16 +2,15 @@ from commands.base_command import BaseCommand
 
 class SearchesCommand(BaseCommand):
     def run(self, update, context):
-        messages = []
         self.config.load()
         providers_config = self.config.get('providers')
         for provider_key in providers_config.keys():
+            messages = []
             messages.append(f"> {provider_key}:")
             provider_config = providers_config[provider_key]
             for source in provider_config['sources']:
                 url = provider_config['base_url'] + source
                 messages.append(f">>> {url}")
-        final_message = '\n'.join(messages)
-        print(final_message)
-        update.message.reply_text(final_message)
+            provider_config_message = '\n'.join(messages)
+            update.message.reply_text(provider_config_message)
 
